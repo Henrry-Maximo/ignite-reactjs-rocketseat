2d625465c76a2e4e419516ae2e19cf4228c39ba3
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-02 11:15:42 -0300

    ✨feat: add component/status at form history

--- 02-ignite-timer/src/pages/History/styles.ts
+++ 02-ignite-timer/src/pages/History/styles.ts
@@ -9,9 +9,9 @@ export const HistoryContainer = styled.main`
 
   h1 {
     font-size: 1.5rem;
-    color: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme["gray-100"]};
   }
-`
+`;
 
 export const HistoryList = styled.div`
   flex: 1;
@@ -24,10 +24,10 @@ export const HistoryList = styled.div`
     min-width: 600px;
 
     th {
-      background-color: ${(props) => props.theme['gray-600']};
+      background-color: ${(props) => props.theme["gray-600"]};
       padding: 1rem;
       text-align: center;
-      color: ${(props) => props.theme['gray-100']};
+      color: ${(props) => props.theme["gray-100"]};
       font-size: 0.875rem;
       line-height: 1.6rem;
 
@@ -43,8 +43,8 @@ export const HistoryList = styled.div`
     }
 
     td {
-      background-color: ${(props) => props.theme['gray-600']};
-      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      background-color: ${(props) => props.theme["gray-600"]};
+      border-top: 4px solid ${(props) => props.theme["gray-800"]};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
@@ -59,6 +59,30 @@ export const HistoryList = styled.div`
       }
     }
   }
-`
+`;
 
+const STATUS_COLORS = {
+  yellow: "yellow-500",
+  green: "green-500",
+  red: "red-500",
+} as const;
 
+interface StatusProps {
+  // statusColor: 'yellow' | 'red' | 'green';
+  statusColor: keyof typeof STATUS_COLORS;
+}
+
+// generics typescript
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: "";
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`;
