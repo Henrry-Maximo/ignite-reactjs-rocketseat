262eee8f502d5661b75bc658fe3f01a4efb28d99
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-22 10:52:30 -0300

    🎨 Styles: Add globals styles in theme provider app

--- ignite-timer-without-styled-components/vite-pure/src/App.tsx
+++ ignite-timer-without-styled-components/vite-pure/src/App.tsx
@@ -2,11 +2,14 @@ import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
 
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
