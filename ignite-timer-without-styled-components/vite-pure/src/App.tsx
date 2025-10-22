66ced26854577dc5ac5968af5d5a16d975d1ba0a
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-22 11:37:03 -0300

    ✨ Add config eslint for app

--- ignite-timer-without-styled-components/vite-pure/src/App.tsx
+++ ignite-timer-without-styled-components/vite-pure/src/App.tsx
@@ -2,7 +2,9 @@ import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
 
 import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+
+// 
 
 export const App = () => {
   return (
