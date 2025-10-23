866403d5deedb1523f6f6a86b9dc15b886c90adf
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-23 13:32:57 -0300

    ✨ Feat: Add default layout, more outlet and browser routes - react router dom

--- ignite-timer-without-styled-components/vite-pure/src/App.tsx
+++ ignite-timer-without-styled-components/vite-pure/src/App.tsx
@@ -1,17 +1,26 @@
-import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components";
 
-import { defaultTheme } from "./styles/themes/default"
+import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-
-// 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { History } from "./pages/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/history" element={<History />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <Button variant="primary" /> */}
 
       <GlobalStyle />
     </ThemeProvider>
-  )
-}
+  );
+};
---
+++ ignite-timer-without-styled-components/vite-pure/src/pages/History.tsx
@@ -0,0 +1,5 @@
+export const History = () => {
+  return (
+    <h1>History</h1>
+  );
+};
