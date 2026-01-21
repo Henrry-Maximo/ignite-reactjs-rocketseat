02c56df7697558bd37fc4c4d251a71606ccaf7b9
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-21 17:15:39 -0300

    ✨ Feat: Add react hook form for create new transactions and form serach input

--- 03-dt-money/src/components/SearchForm/index.tsx
+++ 03-dt-money/src/components/SearchForm/index.tsx
@@ -1,12 +1,48 @@
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
 import { MagnifyingGlass } from "phosphor-react";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+/*
+  ** Pattern Names Functions:
+  * search - automatic
+  * handle - client
+*/
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const { 
+    register,
+    handleSubmit,
+    formState: {
+      isSubmitting
+    }
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    // simular uma promessa - 2 segundos
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="Busque por transações" 
+        {...register("query")}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
