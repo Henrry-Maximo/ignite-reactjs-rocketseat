468ae7dc8fe524d874ba48e64d6b7cb200cae289
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-25 16:31:02 -0300

    ✨ Feat: Add use memo react for verify if is necessary loading component again

--- 03-dt-money/src/components/SearchForm/index.tsx
+++ 03-dt-money/src/components/SearchForm/index.tsx
@@ -5,11 +5,28 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useContextSelector } from "use-context-selector";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { memo } from "react";
 
 /*
   ** Pattern Names Functions:
   * search - automatic
   * handle - client
+  * 
+  * Por que um componente renderiza?
+  * - Hooks changed (mudou estado, contexto, reducer);
+  * - Props changed (mudou propriedades);
+  * - Parent rerendered (componente pai renderizou)
+  * 
+  * Qual o fluxo de renderização?
+  * 1. O React recria o HTML da interface daquele componente
+  * 2. Compara a versão do HTML com a versão anterior
+  * 3. Se mudou alguma coisa, ele reescreve o HTML na tela
+  * 
+  * Memo:
+  * 0. Hooks changed, Props changed (deep comparison)
+  * 0.1: Comparar a versão anterior dos hooks e props
+  * 0.2: Se mudou algo, ele vai permitir a nova renderização
+  * Obs.: extremamente custoso, pode gerar lentidão se usado incorretamente
 */
 
 const searchFormSchema = z.object({
@@ -57,3 +74,5 @@ export function SearchForm() {
     </SearchFormContainer>
   );
 }
+
+// export const SearchForm = memo(SearchFormComponent);
