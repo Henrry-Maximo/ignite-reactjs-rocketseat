4ddd99642a76cc6d2526a5ec29ab1ea7000b0f77
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-27 17:39:58 -0300

    ✨ Feat: Add react router dom and declaration of routes

--- 04-pizzashop-web/src/App.tsx
+++ 04-pizzashop-web/src/App.tsx
@@ -1,11 +1,15 @@
 import './globals.css';
 
-import { Button } from './components/ui/button';
+import { RouterProvider } from 'react-router';
+
+import { router } from './routes';
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <RouterProvider router={router} />
+
+    // <div className="flex h-screen items-center justify-center">
+    //   <Button>Enviar</Button>
+    // </div>
   );
 }
