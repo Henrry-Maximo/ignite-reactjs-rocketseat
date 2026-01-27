b88c13ba0b9eafbf786651567345faacf9c58cf6
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-27 18:31:37 -0300

    🎨  Styles: Add layout of login using tailwindcss and shadcn components

--- 04-pizzashop-web/src/App.tsx
+++ 04-pizzashop-web/src/App.tsx
@@ -1,15 +1,26 @@
 import './globals.css';
 
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router';
 
 import { router } from './routes';
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
 
     // <div className="flex h-screen items-center justify-center">
     //   <Button>Enviar</Button>
     // </div>
   );
 }
+
+/*
+  # Helmet
+  - Serve para passar metatags para os elementos da página, podendo identificar a página passando o valor desejado que será exibido de acordo com a rota
+  - React Helmet Async é um fork do React Helmet
+  - Permite alterar as metatags do React, como o título de cada página.
+*/
