44c43c54e7719ae7fc9af2f355548c560f6bbee8
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-28 17:04:18 -0300

    ✨ Feat: Add react hook form and use form sign

--- 04-pizzashop-web/src/pages/auth/sign-in.tsx
+++ 04-pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,15 +1,35 @@
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const signInForm = z.object({
+  email: z.email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <div>
       <Helmet title="Login" />
       <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex w-87.5 flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
@@ -19,13 +39,19 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          {/* high order function pattern */}
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail: </Label>
-              <Input id="email" type="email" placeholder="Digite aqui..." />
+              <Input
+                id="email"
+                type="email"
+                placeholder="Digite aqui..."
+                {...register('email')}
+              />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
