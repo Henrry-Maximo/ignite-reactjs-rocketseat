a90bff550082d302a82ae1ab375399bbe9b1a797
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-29 16:15:21 -0300

    ✨ Feat: Add filters orders component

---
+++ 04-pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,48 @@
+import { ArrowRight, Search, X } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { TableCell, TableRow } from '@/components/ui/table';
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido.</span>
+        </Button>
+      </TableCell>
+
+      <TableCell className="font-mono text-xs font-medium">
+        435345mkmkermwlekm4ol32mk4ol23m4
+      </TableCell>
+
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="text-muted-foreground font-medium">Pendente</span>
+        </div>
+      </TableCell>
+
+      <TableCell className="font-medium">Henrique Maximo</TableCell>
+
+      <TableCell className="font-medium">R$ 149,48</TableCell>
+
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
