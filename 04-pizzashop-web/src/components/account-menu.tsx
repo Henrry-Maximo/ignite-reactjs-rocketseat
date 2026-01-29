ebb037c593d7853b4511488720e6101deeb04861
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-29 15:18:07 -0300

    ✨ Feat: Add theme toggle and menu of account

---
+++ 04-pizzashop-web/src/components/account-menu.tsx
@@ -0,0 +1,47 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react';
+
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Henrique Maximo</span>
+          <span className="text-muted-foreground text-xs font-normal">
+            Henrrylimadasilva@gmail.com
+          </span>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="cursor-pointer">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- 04-pizzashop-web/src/components/header.tsx
+++ 04-pizzashop-web/src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react';
 
+import { AccountMenu } from './account-menu';
 import { NavLink } from './nav-link';
+import { ModeToggle } from './theme/theme-toggle';
 import { Separator } from './ui/separator';
 
 export function Header() {
@@ -22,6 +24,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ModeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   );
