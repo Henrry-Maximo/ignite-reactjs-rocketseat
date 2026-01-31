d3b8468aad3f6314fd82dcf3b2f36fea7b6e7828
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-31 18:34:27 -0300

    ✨ Feat: Add modal for update of informations about profile

--- 04-pizzashop-web/src/components/ui/button.tsx
+++ 04-pizzashop-web/src/components/ui/button.tsx
@@ -12,6 +12,8 @@ const buttonVariants = cva(
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+        success:
+          'bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80',
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary:
