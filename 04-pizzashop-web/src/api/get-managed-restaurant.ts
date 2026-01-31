7f9a640a094052c66a90fd4c84be848885940749
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-31 18:09:53 -0300

    ✨ Feat: Add loading states for profile

---
+++ 04-pizzashop-web/src/api/get-managed-restaurant.ts
@@ -0,0 +1,24 @@
+import { api } from '@/lib/axios';
+
+interface GetManagedRestaurantResponse {
+  name: string;
+  id: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  description: string | null;
+  managerId: string | null;
+}
+
+export async function getManagedRestaurant() {
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  );
+
+  return response.data;
+}
+
+/*
+ * Formas de tipar o retorno:
+ * : Promise<GetManagedRestaurantResponse>
+ * return response.data as GetManagedRestaurantResponse;
+ */
---
+++ 04-pizzashop-web/src/api/get-profile.ts
@@ -0,0 +1,23 @@
+import { api } from '@/lib/axios';
+
+interface GetProfileResponse {
+  name: string;
+  id: string;
+  email: string;
+  phone: string | null;
+  role: 'manager' | 'customer';
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
+
+export async function getProfile() {
+  const response = await api.get<GetProfileResponse>('/me');
+
+  return response.data;
+}
+
+/*
+ * Formas de tipar o retorno:
+ * : Promise<GetProfileResponse>
+ * return response.data as GetProfileResponse;
+ */
