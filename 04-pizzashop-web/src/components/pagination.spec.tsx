dd43eb0d9e2b0ecf420ad6bd083f1a8696bd7246
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-06 16:34:00 -0300

    ✨ Feat: Add tests for all button of pagination and reset spy function

--- 04-pizzashop-web/src/components/pagination.spec.tsx
+++ 04-pizzashop-web/src/components/pagination.spec.tsx
@@ -6,13 +6,17 @@ import { Pagination } from './pagination';
 const onPageChangeCallback = vi.fn();
 
 describe('Pagination', () => {
+  beforeEach(() => {
+    onPageChangeCallback.mockClear(); // limpar chamadas entre os testes
+  });
+
   it('should display the right amount of pages and results', () => {
     const wrapper = render(
       <Pagination
         pageIndex={0}
         totalCount={200}
         perPage={10}
-        onPageChange={() => {}}
+        onPageChange={onPageChangeCallback}
       />,
     );
 
@@ -40,6 +44,74 @@ describe('Pagination', () => {
 
     // expect(onPageChangeCallback).toHaveBeenCalled();
     expect(onPageChangeCallback).toHaveBeenCalledWith(1);
+  });
+
+  it('should be able to navigate to the previous page', async () => {
+    const user = userEvent.setup();
+
+    console.log(onPageChangeCallback.mock.calls);
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const prevPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior',
+    });
+
+    await user.click(prevPageButton);
+
+    // expect(onPageChangeCallback).toHaveBeenCalled();
+    expect(onPageChangeCallback).toHaveBeenCalledWith(4);
+  });
+
+  it('should be able to navigate to the first page', async () => {
+    const user = userEvent.setup();
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const prevFirstPageButton = wrapper.getByRole('button', {
+      name: 'Primeira página',
+    });
+
+    await user.click(prevFirstPageButton);
+
+    // expect(onPageChangeCallback).toHaveBeenCalled();
+    expect(onPageChangeCallback).toHaveBeenCalledWith(0);
+  });
+
+  it('should be able to navigate to the last page', async () => {
+    const user = userEvent.setup();
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const lastPageButton = wrapper.getByRole('button', {
+      name: 'Última página',
+    });
+
+    await user.click(lastPageButton);
+
+    // expect(onPageChangeCallback).toHaveBeenCalled();
+    expect(onPageChangeCallback).toHaveBeenCalledWith(19);
   });
 });
 
