12be8b4520a620c0930ec672bec83ba9443f8589
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-08 15:36:14 -0300

    ✨ Feat: Add register restaurant mock

--- 04-pizzashop-web/src/api/mocks/index.ts
+++ 04-pizzashop-web/src/api/mocks/index.ts
@@ -2,9 +2,10 @@ import { setupWorker } from 'msw/browser';
 
 import { env } from '@/env';
 
+import { registerRestaurantMock } from './register-restaurant-mock';
 import { signInMock } from './sign-in-mock';
 
-export const worker = setupWorker(signInMock);
+export const worker = setupWorker(signInMock, registerRestaurantMock);
 
 // Apenas quando for chamado, aí os mocks entraram em ação
 export async function enableMSW() {
