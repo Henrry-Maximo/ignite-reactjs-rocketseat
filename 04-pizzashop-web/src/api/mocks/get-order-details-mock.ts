aa6cd9be158608dca7287d9f4d48a9d168d8bb62
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-09 14:14:00 -0300

    ✨ Feat: Add get order details mock

---
+++ 04-pizzashop-web/src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,38 @@
+import { http, HttpResponse } from 'msw';
+
+import type {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details';
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/order/:orderId', ({ params }) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    customer: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '1231241251115',
+    },
+    status: 'pending',
+    createdAt: new Date().toDateString(),
+    totalInCents: 5000,
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 1000,
+        product: { name: 'Pizza Pepperoni' },
+        quantity: 1,
+      },
+      {
+        id: 'order-item-2',
+        priceInCents: 2000,
+        product: { name: 'Pizza Marguerita' },
+        quantity: 2,
+      },
+    ],
+  });
+});
