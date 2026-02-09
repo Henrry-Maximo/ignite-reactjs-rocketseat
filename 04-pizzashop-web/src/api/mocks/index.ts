1781e54ca2795e26f4df04740856533915d316fb
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-09 14:27:17 -0300

    ✨ Feat: Add approve/cancel/deliver and dispatch mocks for actions of status

--- 04-pizzashop-web/src/api/mocks/index.ts
+++ 04-pizzashop-web/src/api/mocks/index.ts
@@ -2,6 +2,10 @@ import { setupWorker } from 'msw/browser';
 
 import { env } from '@/env';
 
+import { approveOrderMock } from './approve-order-mock';
+import { cancelOrderMock } from './cancel-order-mock';
+import { deliverOrderMock } from './deliver-order-mock';
+import { dispatchOrderMock } from './dispatch-order-mock';
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock';
 import { getDayOrdersAmountMock } from './get-day-orders-amout-mock';
 import { getManagedRestaurantMock } from './get-managed-restaurant';
@@ -30,6 +34,10 @@ export const worker = setupWorker(
   updateProfileMock,
   getOrdersMock,
   getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 );
 
 // Apenas quando for chamado, aí os mocks entraram em ação
