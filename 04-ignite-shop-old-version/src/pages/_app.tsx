8ae87a484e3eda1be2a74a870cf636394cf179ca
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-14 23:07:03 -0300

    ✨ Feat: Add component image next for optimize images

--- 04-ignite-shop-old-version/src/pages/_app.tsx
+++ 04-ignite-shop-old-version/src/pages/_app.tsx
@@ -6,13 +6,15 @@ import { globalStyles } from "../styles/global";
 import logoImg from '../assets/logo.svg';
 import { Container, Header } from "../styles/pages/app";
 
+import Image from 'next/image'
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} width={150} height={150} alt="" />
       </Header>
 
       <Component {...pageProps} />
