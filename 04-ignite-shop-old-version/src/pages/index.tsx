2fbb9ee0fc5f9e6e2e7ee37868c8eb0c6b1b07f8
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-15 12:51:52 -0300

    ✨ Feat: Add structure home and styles

--- 04-ignite-shop-old-version/src/pages/index.tsx
+++ 04-ignite-shop-old-version/src/pages/index.tsx
@@ -1,5 +1,39 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="Uma camisa bonita e suave, em cores fortes." />
+
+        <footer>
+          <strong>Camista X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="Uma camisa bonita e suave, em cores fortes." />
+
+        <footer>
+          <strong>Camista X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
+};
+
+/* 
+* File-system Routing: Roteamento baseado em arquivos físicos
+* Encontrar produto para exibição: via parâmetro na URL - ...3000/product/1
+
+- Botão de exemplo
 const Button = styled('button', {
   backgroundColor: '$green300',
   borderRadius: 8,
@@ -15,17 +49,4 @@ const Button = styled('button', {
   }
 
 });
-
-export default function Home() {
-  return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
-  )
-};
-
-/* 
-  * File-system Routing: Roteamento baseado em arquivos físicos
-  * Encontrar produto para exibição: via parâmetro na URL - ...3000/product/1
 */
