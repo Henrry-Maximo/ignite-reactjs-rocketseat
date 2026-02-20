b2f5f9695e8b209b364b66a1320587601ff51333
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-20 14:46:40 -0300

    ✨ Feat: Add prefetch in link

--- 04-ignite-shop-old-version/src/pages/index.tsx
+++ 04-ignite-shop-old-version/src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home({ products }: HomeProps) {
       {
         products.map((product) => {
           return (
-            <Link key={product.id} href={`/product/${product.id}`}>
+            <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
               <Product  className="keen-slider__slide">
                 <Image src={product.imageUrl} width={520} height={480} alt="Uma camisa bonita e suave, em cores fortes." />
 
