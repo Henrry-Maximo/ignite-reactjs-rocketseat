8b687bc0b717b9ba4b420223ac18505c73dcd22d
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-27 14:25:52 -0300

    ✨ Feat: Add config stitches and styles

--- 05-design-system/packages/react/src/index.tsx
+++ 05-design-system/packages/react/src/index.tsx
@@ -1,7 +1,15 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+  height: '$10'
+})
 
 export function App() {
   return (
-    <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+    <Button>Hello World!</Button>
   )
 }
---
+++ 05-design-system/packages/react/src/styles/index.ts
@@ -0,0 +1,20 @@
+import { colors, fonts, fontSizes, fontWeights, lineHeights, radii, space } from "@ignite-ui/tokens";
+import { createStitches, defaultThemeMap } from "@stitches/react";
+
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } =createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space'
+  },
+
+  theme: {
+    colors: colors,
+    fontSizes: fontSizes,
+    fontWeights: fontWeights,
+    fonts: fonts,
+    lineHeights: lineHeights,
+    radii: radii,
+    space: space
+  }
+});
