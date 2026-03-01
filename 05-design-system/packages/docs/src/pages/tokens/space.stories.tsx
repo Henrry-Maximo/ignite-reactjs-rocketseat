459812dee85acb624a2a4450aec93b6a318b04e0
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-03-01 17:54:32 -0300

    ✨ Feat: Add space grid tokens

---
+++ 05-design-system/packages/docs/src/pages/tokens/space.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TokensGrid } from '../../components/TokensGrid'
+import { space } from '@ignite-ui/tokens'
+
+export default {
+  title: 'Tokens/Spaces',
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+  },
+} as Meta
+
+export const Page: StoryObj = {
+  render: () => {
+    return (
+      <>
+        <h1>Space</h1>
+        
+        <TokensGrid tokens={space} />
+      </>
+    )
+  },
+}
