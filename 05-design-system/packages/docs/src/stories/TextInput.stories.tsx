d0bc8fa30f2e05348869037bd1a12ddddf83c641
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-03-04 14:41:02 -0300

    ✨ Feat: Add text area component

---
+++ 05-design-system/packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,41 @@
+import type { StoryObj, Meta } from '@storybook/react-vite'
+import { Box, Text, TextInput, type TextInputProps } from "@ignite-ui/react";
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box>
+          <Text 
+            as='label' 
+            css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+          >
+            Email address
+          </Text>
+          {Story()}
+        </Box>
+      );
+    }
+  ]
+} as Meta<TextInputProps>;
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name'
+  }
+};
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true
+  }
+};
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  }
+};
