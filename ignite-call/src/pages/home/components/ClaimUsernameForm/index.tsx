257dce5084c3f690d80b88833d2eadd5b3ef1809
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-03-15 21:31:33 -0300

    ✨ feat: Add register username input react hook form

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,16 +1,31 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { Form } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data.username);
+  };
+
   return (
-    <Form as='form'>
+    <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size='sm'
         prefix='ignite.com/'
         placeholder='seu-usuario'
+        { ...register('username') }
       />
-      <Button size='sm' type="submit">Reservar usuário <ArrowRight /></Button>
+      <Button size='sm' type="submit">Reservar<ArrowRight></ArrowRight></Button>
     </Form>
   )
 }
