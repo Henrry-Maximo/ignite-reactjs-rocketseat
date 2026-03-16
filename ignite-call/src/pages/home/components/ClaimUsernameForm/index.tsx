4460eeb742adbf3905b90065ef8e3da470a2c2b3
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-03-15 22:29:45 -0300

    ✨ feat: Add validation schema zod username

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,17 +1,27 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { Form } from "./styles";
+import { Button, Text, TextInput } from "@ignite-ui/react";
+import { Form, FormAnnotation } from "./styles";
 import { ArrowRight } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
 
 const claimUsernameFormSchema = z.object({
-  username: z.string(),
+  username: z.string()
+    .min(3, {
+      message: 'O usuário precisa ter pelo menos 3 letras.'
+    })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.'
+    })
+    .transform(username => username.toLocaleLowerCase()),
 });
 
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+  const { register, handleSubmit, formState: { errors } } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  });
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data.username);
@@ -23,9 +33,15 @@ export function ClaimUsernameForm() {
         size='sm'
         prefix='ignite.com/'
         placeholder='seu-usuario'
-        { ...register('username') }
+        {...register('username')}
       />
       <Button size='sm' type="submit">Reservar<ArrowRight></ArrowRight></Button>
+      
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username ? errors.username.message : 'Digite o nome do usuário desejado.'}
+        </Text>
+      </FormAnnotation>
     </Form>
   )
 }
