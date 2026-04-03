bbfa1db6ec3e4996aaa33477dd056e0617e72d5f
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-03 20:37:21 -0300

    ✨ feat: Add time intervals react hook form and array with days of week

--- ignite-call/src/pages/register/time-intervals/index.page.tsx
+++ ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -2,8 +2,60 @@ import { Button, Checkbox, Heading, MultiStep, Text, TextInput } from "@ignite-u
 import { Container, Header } from "../styles";
 import { IntervalBox, IntervalDay, IntervalInputs, IntervalItem, IntervalsContainer } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useFieldArray, useForm } from "react-hook-form";
+import z from "zod";
+import { getWeekDays } from "../../../utils/get-week-days";
+
+const timeIntervalsFormSchema = z.object({
+
+});
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: {
+      isSubmitting,
+      errors
+    }
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ]
+    }
+  });
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  });
+
+  async function handleSetTimeIntervals() {
+
+  }
+
+  const weekDays = getWeekDays();
+
+  /**
+   * React Field Array - React Hook Form 
+   * [
+   *   { day: 0, start: time, end: time },
+   *   { day: 1, start: time, end: time },
+   *   { day: 2, start: time, end: time },
+   *   { day: 3, start: time, end: time },
+   *   { day: 4, start: time, end: time },
+   *   { day: 5, start: time, end: time },
+   *   { day: 6, start: time, end: time },
+   * ] 
+  */
 
   return (
     <Container>
@@ -17,49 +69,34 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <IntervalBox as='form'>
+      <IntervalBox as='form' onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalsContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Checkbox />
+                  <Text>{weekDays[field.weekDay]}</Text>
+                </IntervalDay>
 
-            <IntervalInputs>
-              <TextInput 
-                size="sm"
-                type="time"
-                step={60}
-              />
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    { ...register(`intervals.${index}.startTime`) }
+                  />
 
-              <TextInput 
-                size="sm"
-                type="time"
-                step={60}
-              />
-            </IntervalInputs>
-          </IntervalItem>
-
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Terça-feira</Text>
-            </IntervalDay>
-
-            <IntervalInputs>
-              <TextInput 
-                size="sm"
-                type="time"
-                step={60}
-              />
-
-              <TextInput 
-                size="sm"
-                type="time"
-                step={60}
-              />
-            </IntervalInputs>
-          </IntervalItem>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    { ...register(`intervals.${index}.endTime`) }
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
         </IntervalsContainer>
 
         <Button type="submit">
