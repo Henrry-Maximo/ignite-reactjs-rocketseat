7f3d5b8a3e3d25786bdf8c79cc891279e9cf8b96
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-03 20:36:18 -0300

    ✨ feat: Add get week days function util

---
+++ ignite-call/src/utils/get-week-days.ts
@@ -0,0 +1,24 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-br', { weekday: 'long' });
+
+  /**
+   * [ 0, 1, 2, 3, 4, 5, 6 ]
+   */
+
+  return Array.from(
+    Array(7).keys()
+  )
+  .map((day) => 
+    formatter.format(new Date(
+      Date.UTC(2021, 5, day)
+    )
+  ))
+  .map((weekDay) => {
+    return weekDay
+      .substring(0, 1)
+      .toUpperCase()
+      .concat(
+        weekDay.substring(1)
+      );
+  })
+}
