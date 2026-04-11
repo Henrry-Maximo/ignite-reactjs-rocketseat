0c9d3559a85a741bea10bd59b4835503b8123286
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-11 15:53:29 -0300

    ✨ feat: Add structure calendar step, confirm step and calendar component

--- ignite-call/src/utils/get-week-days.ts
+++ ignite-call/src/utils/get-week-days.ts
@@ -1,4 +1,9 @@
-export function getWeekDays() {
+
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-br', { weekday: 'long' });
 
   /**
@@ -8,17 +13,23 @@ export function getWeekDays() {
   return Array.from(
     Array(7).keys()
   )
-  .map((day) => 
-    formatter.format(new Date(
-      Date.UTC(2021, 5, day)
-    )
-  ))
-  .map((weekDay) => {
-    return weekDay
-      .substring(0, 1)
-      .toUpperCase()
-      .concat(
-        weekDay.substring(1)
-      );
-  })
+    .map((day) =>
+      formatter.format(new Date(
+        Date.UTC(2021, 5, day)
+      )
+      ))
+    .map((weekDay) => {
+      if (short) {
+        return weekDay
+          .substring(0, 3)
+          .toUpperCase();
+      }
+
+      return weekDay
+        .substring(0, 1)
+        .toUpperCase()
+        .concat(
+          weekDay.substring(1)
+        );
+    })
 }
