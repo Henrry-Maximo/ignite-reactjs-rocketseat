dacbfb2b868bd46a690ea85f132e94f7970883f1
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-04-13 12:22:46 -0300

    ✨ feat: Add schedule confirm step page and styles

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,7 +1,44 @@
-import { CalendarStep } from "../CalendarStep";
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { CalendarBlank, Clock } from "phosphor-react";
 
-export function ScheduleForm() {
+export function ConfirmStep() {
+  function handleConfirmScheduling() {
+
+  };
+
   return (
-    <CalendarStep />
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de E-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">Cancelar</Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
   )
 }
--- ignite-call/src/pages/schedule/[username]/index.page.tsx
+++ ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -2,7 +2,7 @@ import { Avatar, Heading, Text } from "@ignite-ui/react";
 import { Container, UserHeader } from "./styles";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { prisma } from "../../../lib/prisma";
-import { ScheduleForm } from "./ScheduleForm/ConfirmStep";
+import { ScheduleForm } from "./ScheduleForm";
 
 interface ScheduleProps {
   user: {
