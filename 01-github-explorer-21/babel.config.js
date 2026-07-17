e74d18f14eabb7d2c653e19ebba67d7cbff316b4
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-07-17 01:46:40 -0300

    🐍 fix: Add adjust in import react babel and add commands for start application

--- 01-github-explorer-21/babel.config.js
+++ 01-github-explorer-21/babel.config.js
@@ -1,6 +1,10 @@
+const { runtime } = require("webpack");
+
 module.exports = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react'
+    ['@babel/preset-react', {
+      runtime: 'automatic'
+    }]
   ]
 }
--- 01-github-explorer-21/src/index.jsx
+++ 01-github-explorer-21/src/index.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+// import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
+
+render(<App />, document.getElementById('root'))
 
 // function App() {
 //   return <h1>Hellow World</h1>
