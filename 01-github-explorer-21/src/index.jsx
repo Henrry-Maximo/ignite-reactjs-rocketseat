92e5158f77a9e08ce9a176be0f7759471244f7e3
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-07-17 01:30:20 -0300

    ✨ feat: Add babel and webpack config on react project

---
+++ 01-github-explorer-21/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { App } from './App';
+
+// function App() {
+//   return <h1>Hellow World</h1>
+// }
+
+// const user = {
+//   name: "Henrry",
+// }
+
+// console.log(user.address?.street);
