7b20343ad3c262c1717540ecc4f829ff141d6062
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-07-22 12:31:31 -0300

    🐍 fix: Add adjust at dependencies and webpack

--- 01-github-explorer-21/src/App.jsx
+++ 01-github-explorer-21/src/App.jsx
@@ -1,7 +1,5 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
-
 
 export function App() {
   // throw new Error("Catch me");
